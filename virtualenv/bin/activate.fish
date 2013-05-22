4654ee9347e0b35a20e13eef5fb816a8ed7695cb
benvand <benjamin_vandersteen@hotmail.com>
2013-05-22 16:10:08 +1000

    env and schema

--- virtualenv/bin/activate.fish
+++ virtualenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ben/sites/iq/virtualenv"
+set -gx VIRTUAL_ENV "/home/ben/iq/virtualenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
